9586412b721c00d1f4227bd39f86c6b20db516d6
Ali-DEV2 <48789118+Ali-DEV2@users.noreply.github.com>
2019-03-21 18:37:29 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.login('NTU4MzA4MjgxNjA5NzQ4NDkw.D3U-pg.LIU5efexF8YpCrwjFHLN2zXvhUo');
